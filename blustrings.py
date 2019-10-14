2e7b5ef719f3005deb111f8ea28793eb3aab29c7
anurag-11th <38474933+anurag-11th@users.noreply.github.com>
2019-10-14 22:16:36 +0530

    Add files via upload

--- blustrings.py
+++ blustrings.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/LOGO")
 def LOGO():
-    return render_template("sample.html")
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
